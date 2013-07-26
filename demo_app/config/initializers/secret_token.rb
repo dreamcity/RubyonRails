a998b02a8f3647553431c30081780b93559137ca
dreamcity <dreamscity.com>
2013-07-26 16:49:33 +0800

    firstweb

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'ad5543a4816d94cd2d3e2f509e2df14fffbf330e1f898a373c0b7cb34b0626cc69bc95fa71de900dd77a58d0ced344a70159d7823da72d1b6f908fad40fb7a33'
